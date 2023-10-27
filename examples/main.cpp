4cbd75b77cfb09bb07279ce948a9753267d17355
Ahmad Faisal <ahmad.faisalewy@gmail.com>
2023-10-27 22:24:38 +0700

    completed enigma implementation

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,12 +1,67 @@
+/*
+  Reflector : A
+  Rotors    : I-II-III
+  Plugboard : A-R, G-K, O-X
+  Message   : A ==> X
+*/
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include "enigma-emulator/keyboard.h"
+#include <utility>
+#include "enigma-machine-emulator/keyboard.h"
+#include "enigma-machine-emulator/plugboard.h"
+#include "enigma-machine-emulator/rotor.h"
+#include "enigma-machine-emulator/reflector.h"
 
 int main(int argc, char *argv[]){
+
+  if(argc != 2){
+    std::cout << "Usage:./enigma-machine-emulator <string>" << std::endl;
+    return 0;
+  }
+  // signal initialization
+  int signal;
+
+
+  // keyboard initialization
   Keyboard keyboard;
-  std::cout << keyboard.forward('Z') << std::endl;
-  std::cout << keyboard.backward(25) << std::endl;
-  keyboard.printHello();
+
+  // plugboard initialization
+  Plugboard plugboard(std::vector<std::pair<char, char>>{std::make_pair('A', 'R'), std::make_pair('G', 'K'), std::make_pair('O', 'X')});
+
+  // Rotor initialization
+  Rotor I("EKMFLGDQVZNTOWYHXUSPAIBRCJ", 'Q');
+  Rotor II("AJDKSIRUXBLHWTMCQGZNPYFVOE", 'E');
+  Rotor III("BDFHJLCPRTXVZNYEIWGAKMUSQO", 'V');
+  Rotor IV("ESOVPZJAYQUIRHXLNFTGKDCMWB", 'J');
+  Rotor V("VZBRGITYUPSDNHLXAWMJQOFECK", 'Z');
+
+  // Reflector initialization
+  Reflector reflector("EJMZALYXVBWFCRQUONTSPIKHGD");
+
+  for(char character : std::string(argv[1])){
+    char letter = character;
+
+    signal = keyboard.forward(letter);
+    signal = plugboard.forward(signal);
+    signal = V.forward(signal);
+    signal = IV.forward(signal);
+    signal = III.forward(signal);
+    signal = II.forward(signal);
+    signal = I.forward(signal);
+    signal = reflector.reflect(signal);
+    signal = I.backward(signal);
+    signal = II.backward(signal);
+    signal = III.backward(signal);
+    signal = IV.backward(signal);
+    signal = V.backward(signal);
+    signal = plugboard.backward(signal);
+    letter = keyboard.backward(signal);
+
+    std::cout << letter;
+  }
+
+
   return 0;
 }
